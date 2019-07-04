e2d70752a1756af7181bcda50492f7e2695fcad5
Reinier Mujica <reinier.mujica@gmail.com>
2019-07-05 01:51:07 +0200

    Reduced months

--- src/helpers/Charts.js
+++ src/helpers/Charts.js
@@ -2,15 +2,26 @@ module.exports.parseBarChartData = (tweetsByMonth) => {
   if (!tweetsByMonth) return [];
 
   const data = makeMockData();
+  const parsedData = removeMonths(tweetsByMonth);
 
-  data.series = [...tweetsByMonth];
+  data.series = [...parsedData];
 
   return data;
 };
 
 function makeMockData() {
   return {
-    labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    labels: ['Feb', 'Mar', 'Abr', 'May', 'Jun'],
     series: [],
   };
+}
+
+function removeMonths(series) {
+  if (series.length === 12 && typeof series[0] === 'number') {
+    return series.slice(1, 6);
+  }
+
+  return series.map((serie) => {
+    return serie.slice(1, 6);
+  });
 }
